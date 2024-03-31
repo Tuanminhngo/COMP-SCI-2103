547df790718e4235d5d2f222527e1beb9ddcc506
Tuanminhngo <a1869387@adelaide.edu.au>
2024-03-31 22:13:36 +1030

    asdfadf

--- Finder.cpp
+++ Finder.cpp
@@ -15,15 +15,17 @@ using namespace std;
 //   }
 //   return result;
 // }
+
 vector<int> Finder::findSubstrings(string s1, string s2) {
-    vector<int> result(s2.size(), -1);
+    vector<int> result;
     for (size_t i = 1; i <= s2.size(); i++) {
-        size_t found = s1.find(s2.substr(0, i));
+        string prefix = s2.substr(0, i);
+        size_t found = s1.find(prefix);
         if (found != string::npos) {
-            result[i - 1] = found;
+            result.push_back(found);
+        } else {
+            result.push_back(-1);
         }
     }
     return result;
 }
-
-
